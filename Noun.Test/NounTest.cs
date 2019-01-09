433c39fbd8263777371ab85b35fff59c697a89a0
Akhmed Sheriev <sheriev@interfax.ru>
2019-01-09 16:32:55 +0300

    fix unicode

--- Noun.Test/NounTest.cs
+++ Noun.Test/NounTest.cs
@@ -1,4 +1,4 @@
-using System;
+п»їusing System;
 using Xunit;
 
 namespace NounHelper.Test
@@ -6,36 +6,36 @@ namespace NounHelper.Test
     public class NounTest
     {
         [Theory]
-        [InlineData(0, "источников")]
-        [InlineData(1, "источник")]
-        [InlineData(2, "источника")]
-        [InlineData(3, "источника")]
-        [InlineData(4, "источника")]
-        [InlineData(5, "источников")]
-        [InlineData(6, "источников")]
-        [InlineData(7, "источников")]
-        [InlineData(8, "источников")]
-        [InlineData(9, "источников")]
-        [InlineData(10, "источников")]
-        [InlineData(11, "источников")]
-        [InlineData(12, "источников")]
-        [InlineData(13, "источников")]
-        [InlineData(14, "источников")]
-        [InlineData(15, "источников")]
-        [InlineData(16, "источников")]
-        [InlineData(17, "источников")]
-        [InlineData(19, "источников")]
-        [InlineData(20, "источников")]
-        [InlineData(21, "источник")]
-        [InlineData(55,  "источников")]
-        [InlineData(101, "источник")]
-        [InlineData(111, "источников")]
-        [InlineData(112, "источников")]
+        [InlineData(0, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(1, "РёСЃС‚РѕС‡РЅРёРє")]
+        [InlineData(2, "РёСЃС‚РѕС‡РЅРёРєР°")]
+        [InlineData(3, "РёСЃС‚РѕС‡РЅРёРєР°")]
+        [InlineData(4, "РёСЃС‚РѕС‡РЅРёРєР°")]
+        [InlineData(5, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(6, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(7, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(8, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(9, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(10, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(11, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(12, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(13, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(14, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(15, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(16, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(17, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(19, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(20, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(21, "РёСЃС‚РѕС‡РЅРёРє")]
+        [InlineData(55,  "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(101, "РёСЃС‚РѕС‡РЅРёРє")]
+        [InlineData(111, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
+        [InlineData(112, "РёСЃС‚РѕС‡РЅРёРєРѕРІ")]
         public void TestNoun(int number, string expected)
         {
-            var f0 = "источников";
-            var f1 = "источник";
-            var f2 = "источника";
+            var f0 = "РёСЃС‚РѕС‡РЅРёРєРѕРІ";
+            var f1 = "РёСЃС‚РѕС‡РЅРёРє";
+            var f2 = "РёСЃС‚РѕС‡РЅРёРєР°";
 
             var actual = Noun.NounHelper.GetDeclension(number, f1, f2, f0);
             Assert.Equal(expected, actual);
